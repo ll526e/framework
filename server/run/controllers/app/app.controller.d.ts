98569c18e68076dc01d348e1e15762f995ee4b50
ll526e <ll526e@163.com>
2023-02-22 16:30:47 +0800

    feat: del run

--- server/run/controllers/app/app.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { AppService } from '@service/app.service';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
-    getName(): string;
-}
--- server/run/controllers/app/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { AppService } from '@service/app';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
-    getName(): string;
-}
