d8306c5e6599ad5a9d03aa5aef6009a0131c53ad
MY ZHANG <ll526e@163.com>
2023-02-27 10:54:37 +0800

    feat: some change

--- web/frame/component/footer-bar.tsx
+++ web/frame/component/footer-bar.tsx
@@ -4,7 +4,9 @@ const FooterBar = () => {
   return (
     <>
       <div className="m-footer">
-        Copyright © 2022 - {newYear} <em className='m-footer-icp'>ICP备案：豫ICP备2022028308</em>
+        Copyright © 2022 - {newYear} <em className='m-footer-icp'>ICP备案：
+          <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">豫ICP备2022028308号-1</a>
+        </em>
       </div>
     </>
   )
